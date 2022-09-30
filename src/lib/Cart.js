af82096cd81b5f6cdc2f8a1a732d5d908d07dc17
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2022-09-29 21:07:56 -0300

    add more unit test

---
+++ src/lib/Cart.js
@@ -0,0 +1,62 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+
+import Money from 'dinero.js';
+
+// const Money = dinero;
+
+Money.defaultCurrency = 'BRL';
+Money.defaultPrecision = 2;
+
+export default class Cart {
+  items = [];
+
+  add(item) {
+    const itemToFind = { product: item.product };
+
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+    this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      const amount = Money({ amount: item.quantity * item.product.price });
+      let discount = Money({ amount: 0 });
+
+      if (
+        item.condition &&
+        item.condition.percentage &&
+        item.quantity > item.condition.minimum
+      ) {
+        discount = amount.percentage(item.condition.percentage);
+      }
+      return acc.add(amount).subtract(discount);
+    }, Money({ amount: 0 }));
+  }
+
+  summary() {
+    const total = this.getTotal().getAmount();
+    const items = this.items;
+
+    return {
+      total,
+      items,
+    };
+  }
+
+  checkout() {
+    const { total, items } = this.summary();
+
+    this.items = [];
+    return {
+      total,
+      items,
+    };
+  }
+}
---
+++ src/lib/Cart.spec.js
@@ -0,0 +1,122 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+  let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388,
+  };
+
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872,
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  describe('getTotal()', () => {
+    it('should return 0 whe getTotal() is executed in a newly created instance ', () => {
+      expect(cart.getTotal().getAmount()).toEqual(0);
+    });
+
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2, //70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should ensure no more than one product exists at a time ', () => {
+      cart.add({
+        product,
+        quantity: 2, //70776
+      });
+
+      cart.add({
+        product,
+        quantity: 1, //70776
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then remove', () => {
+      cart.add({
+        product,
+        quantity: 2, //70776
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1, //70776
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal().getAmount()).toEqual(41872);
+    });
+  });
+
+  describe('checkout()', () => {
+    it('should return an object with the total and list of items', () => {
+      cart.add({
+        product,
+        quantity: 2, //70776
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3, //70776
+      });
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('should return an object with the total and list of items when summary() is called', () => {
+      cart.add({
+        product,
+        quantity: 2, //70776
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3, //70776
+      });
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+    });
+
+    it('should reset the cart when checkout', () => {
+      cart.add({
+        product: product2,
+        quantity: 3, //70776
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal().getAmount()).toEqual(0);
+    });
+  });
+
+  describe('special conditions', () => {
+    it('should apply percentage discount above minimum is passed', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 3,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(74315);
+    });
+  });
+});
